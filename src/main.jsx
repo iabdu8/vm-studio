ce4d2b351cdf25289b5eb2959861adddcd611f84
iabdu8 github <abduahmed486@gmail.com github>
2026-06-12 03:55:42 +0300

    add ThemeProvider to main

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { AppProvider } from "./context/AppContext.jsx";
+import { ThemeProvider } from "./context/ThemeContext.jsx";
 import App from "./App.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <AppProvider>
-      <App />
-    </AppProvider>
+    <ThemeProvider>
+      <AppProvider>
+        <App />
+      </AppProvider>
+    </ThemeProvider>
   </React.StrictMode>
 );
 
-// PWA service worker
 if ("serviceWorker" in navigator) {
   window.addEventListener("load", () => {
     navigator.serviceWorker.register("/sw.js").catch(() => {});
